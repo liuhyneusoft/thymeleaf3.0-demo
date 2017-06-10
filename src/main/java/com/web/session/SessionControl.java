d62dacad4b438489755ae8b8955c9d46475a8ff9
Ocean <176971923@qq.com>
2017-06-10 18:56:11 +0800

    get something from session

---
+++ src/main/java/com/web/session/SessionControl.java
@@ -0,0 +1,27 @@
+package com.web.session;
+
+import com.web.User;
+import com.web.UserEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by Administrator on 2017/6/10.
+ */
+@Controller
+@RequestMapping("/session")
+public class SessionControl {
+
+    @RequestMapping("s1")
+    public String s1(HttpSession session){
+        session.setAttribute("userinfo","hahaha");
+        session.setAttribute("userObject",new UserEntity(4,"djlkds","8888","ddd"));
+        return "/sessions/index";
+    }
+
+    public String getFromSpringBean(){
+        return "go go go ";
+    }
+}
