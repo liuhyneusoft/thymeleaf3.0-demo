7795fe7a32755db66f25222102d25b474bdd694f
176971923@qq.com <176971923@qq.com>
2017-06-14 10:22:04 +0800

    selecter linked

--- src/main/java/com/web/layout/LayoutDemo.java
+++ src/main/java/com/web/layout/LayoutDemo.java
@@ -1,15 +1,19 @@
 package com.web.layout;
 
+import com.web.SelectItem;
 import com.web.UserEntity;
 import com.web.demo.DemoForm;
 import com.web.demo.DemoGrid;
 import com.web.demo.GridHeadSelected;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class LayoutDemo {
@@ -50,6 +54,29 @@ public class LayoutDemo {
 
         map.addAttribute("user",new UserEntity());//给dialog用
 
+        List<SelectItem> selectList = new ArrayList<>();
+        selectList.add(new SelectItem("key1","value1"));
+        selectList.add(new SelectItem("key2","value2"));
+        selectList.add(new SelectItem("key3","value3"));
+        map.addAttribute("selectList",selectList);
+
         return "/layout/demo1";
+    }
+
+
+    @RequestMapping("/layout/change/{key}")
+    @ResponseBody
+    public  List<SelectItem> layoutChange(@PathVariable("key")String key){
+        List<SelectItem> selectList = new ArrayList<>();
+        if(Objects.equals(key,"key2")){
+            selectList.add(new SelectItem("keyaaaaa","value111111"));
+            selectList.add(new SelectItem("keybbbbb","value222222"));
+            selectList.add(new SelectItem("keyccccc","value333333"));
+        }else if (Objects.equals(key,"bbbb")){
+            selectList.add(new SelectItem("keydddddd","value44444"));
+            selectList.add(new SelectItem("keyeeeeee","value55555"));
+            selectList.add(new SelectItem("keyffffff","value666666"));
+        }
+        return selectList;
     }
 }
